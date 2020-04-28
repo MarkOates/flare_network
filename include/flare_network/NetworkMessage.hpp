90ace8f7067600a43d08a2a275838f1f67a4c6e6
Mark Oates <markoates0@gmail.com>
2020-04-28 00:17:50 -0400

    Extract header/source for NetworkMessage

--- include/flare_network/NetworkMessage.hpp
+++ include/flare_network/NetworkMessage.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
+
 
 class NetworkMessage
 {
@@ -18,87 +18,20 @@ private:
 
 public:
 
-   NetworkMessage()
-      : body_length_(0)
-     , sender_id(0)
-     , recipient_id(0)
-   {
-   }
-
-   const char* data() const
-   {
-      return data_;
-   }
-
-   char* data()
-   {
-      return data_;
-   }
-
-   std::size_t length() const
-   {
-      return header_length + body_length_;
-   }
-
-   const char* body() const
-   {
-      return data_ + header_length;
-   }
-
-   char* body()
-   {
-      return data_ + header_length;
-   }
-
-   std::size_t body_length() const
-   {
-      return body_length_;
-   }
-
-   void body_length(std::size_t new_length)
-   {
-      body_length_ = new_length;
-      if (body_length_ > max_body_length)
-         body_length_ = max_body_length;
-   }
-
-   bool decode_header()
-   {
-      char header[header_length + 1] = "";
-      std::strncat(header, data_, header_length);
-      body_length_ = std::atoi(header);
-      if (body_length_ > max_body_length)
-      {
-         body_length_ = 0;
-         return false;
-      }
-      return true;
-   }
-
-   void encode_header()
-   {
-      char header[header_length + 1] = "";
-      std::sprintf(header, "%4d", static_cast<int>(body_length_));
-      std::memcpy(data_, header, header_length);
-   }
-
-   int get_recipient_id() const
-   {
-      return recipient_id;
-   }
-
-   void set_recipient_id(int intended_recipient_id)
-   {
-      recipient_id = intended_recipient_id;
-   }
-
-   int get_sender_id() const
-   {
-      return sender_id;
-   }
-
-   void set_sender_id(int new_sender_id)
-   {
-      sender_id = new_sender_id;
-   }
+   NetworkMessage();
+   const char* data() const;
+   char* data();
+   std::size_t length() const;
+   const char* body() const;
+   char* body();
+   std::size_t body_length() const;
+   void body_length(std::size_t new_length);
+   bool decode_header();
+   void encode_header();
+   int get_recipient_id() const;
+   void set_recipient_id(int intended_recipient_id);
+   int get_sender_id() const;
+   void set_sender_id(int new_sender_id);
 };
+
+
---
+++ src/flare_network/NetworkMessage.cpp
@@ -0,0 +1,106 @@
+
+
+#include <flare_network/NetworkMessage.hpp>
+
+#include <cstring> // for strncat and memcpy
+#include <cstdlib> // for std::atoi
+
+
+NetworkMessage::NetworkMessage()
+   : body_length_(0)
+  , sender_id(0)
+  , recipient_id(0)
+{
+}
+
+
+const char* NetworkMessage::data() const
+{
+   return data_;
+}
+
+
+char* NetworkMessage::data()
+{
+   return data_;
+}
+
+
+std::size_t NetworkMessage::length() const
+{
+   return header_length + body_length_;
+}
+
+
+const char* NetworkMessage::body() const
+{
+   return data_ + header_length;
+}
+
+
+char* NetworkMessage::body()
+{
+   return data_ + header_length;
+}
+
+
+std::size_t NetworkMessage::body_length() const
+{
+   return body_length_;
+}
+
+
+void NetworkMessage::body_length(std::size_t new_length)
+{
+   body_length_ = new_length;
+   if (body_length_ > max_body_length)
+      body_length_ = max_body_length;
+}
+
+
+bool NetworkMessage::decode_header()
+{
+   char header[header_length + 1] = "";
+   std::strncat(header, data_, header_length);
+   body_length_ = std::atoi(header);
+   if (body_length_ > max_body_length)
+   {
+      body_length_ = 0;
+      return false;
+   }
+   return true;
+}
+
+
+void NetworkMessage::encode_header()
+{
+   char header[header_length + 1] = "";
+   std::sprintf(header, "%4d", static_cast<int>(body_length_));
+   std::memcpy(data_, header, header_length);
+}
+
+
+int NetworkMessage::get_recipient_id() const
+{
+   return recipient_id;
+}
+
+
+void NetworkMessage::set_recipient_id(int intended_recipient_id)
+{
+   recipient_id = intended_recipient_id;
+}
+
+
+int NetworkMessage::get_sender_id() const
+{
+   return sender_id;
+}
+
+
+void NetworkMessage::set_sender_id(int new_sender_id)
+{
+   sender_id = new_sender_id;
+}
+
+
